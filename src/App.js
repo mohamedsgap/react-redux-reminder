71967bdfcfb3662e623372fe48a2d995060a4439
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-02-10 17:17:22 +0200

    Create react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+          <h1>Build a reminder app with react and redux</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;

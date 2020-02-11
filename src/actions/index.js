9863925d04e98fe8f29dafc29851645d1649818b
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-02-11 10:34:48 +0200

    Add clear reminder action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,19 @@
 import ADD_REMINDER from '../types';
+import {CLEAR_REMINDER} from '../types';
 
 export const addReminder = (text, date) => {
     const action = {
         type: ADD_REMINDER,
         text,
         date
+    }
+    return action
+}
+
+export const clearReminder = (id) => {
+    const action = {
+        type: CLEAR_REMINDER,
+        id
     }
     return action
 }

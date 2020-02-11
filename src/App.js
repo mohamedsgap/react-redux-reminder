9659f01a8b96710371a59144d20a7c6c47c13cd4
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-02-11 09:57:54 +0200

    Feat: renderReminder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
 import React from 'react';
 import './App.css';
+import {addReminder} from './actions'
+import {connect} from  'react-redux'
 
+class  App extends React.Component {
+  state = {
+    text: '',
+    date: new Date()
+  }
 
-function App() {
-  return (
-    <div>
-      <div className="App">
-          <h1>Build a reminder app with react and redux</h1>
+  renderReminders = () => {
+    const {reminder} = this.props
+    return (
+      <ul className="list-group">
+          {
+         reminder.map(remind => {
+              return (
+                <li key={remind.id} className="list-group-item">
+                  <div>{remind.text}</div>
+                  <div>{remind.date}</div>
+                </li>
+                )
+              })
+            }
+      </ul> 
+    )
+
+  }
+
+  render(){
+    return( 
+      <div>
+        <div className="App">
+        <img src=""  />
+        <div className="app-title">
+          <h2>What's in your bucket list?</h2>
+        </div>
+        <input onChange={(e) => this.setState({text: e.target.value})} className="form-control" type="text" placeholder="What you want TODO?" />
+        <input  onChange={(e) => this.setState({date: e.target.value})} className="form-control" type="datetime-local" />
+        <button onClick={()=> this.props.addReminder(this.state.text, this.state.date)} className="btn btn-primary btn-block">ADD</button>
+        {this.renderReminders()}
+        <button className="btn btn-danger btn-block">CLEAR</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+    }
 }
-
-export default App;
+/*
+function mapDispatchToProps(dispatch){
+  return {
+    addReminder : () => dispatch(addReminder())
+  }
+}
+*/
+/*
+function mapStateToProps(state) {
+  return {
+    reminder : state
+  }
+}
+*/
+export default connect(state => {return {reminder : state}}, {addReminder})(App);

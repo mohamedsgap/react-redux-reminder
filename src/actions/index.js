faa4e213c493f4abf66264f27ce5ce4486e1f1a9
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-02-11 10:56:34 +0200

    Clear-All reminder

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import ADD_REMINDER from '../types';
-import {CLEAR_REMINDER} from '../types';
+import {CLEAR_REMINDER, CLEAR_ALL_REMINDER } from '../types';
 
 export const addReminder = (text, date) => {
     const action = {
@@ -14,6 +14,13 @@ export const clearReminder = (id) => {
     const action = {
         type: CLEAR_REMINDER,
         id
+    }
+    return action
+}
+
+export const clearAllReminder = () => {
+    const action = {
+        type: CLEAR_ALL_REMINDER
     }
     return action
 }
